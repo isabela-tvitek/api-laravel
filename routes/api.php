c90f2bf03f86884e1dd4c285a84b8eda413bab45
Isabela Taques Vitek <isat12vitek@gmail.com>
2024-02-25 17:30:56 -0300

    correction

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,6 @@ use App\Http\Controllers\PipelineController;
 |
 */
 
-// Route::middleware('auth:api')->get('/register', function (Request $request) {
-//     return $request->user();
-// });
-
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
